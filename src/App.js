9b768d4d60357293fd7e4633b07718c5385c6087
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-07-15 10:11:33 -0400

    initial

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          PTCG Legends 3.0
+        </p>
+        Coming Soon!
+      </header>
+    </div>
+  );
+}
+
+export default App;
