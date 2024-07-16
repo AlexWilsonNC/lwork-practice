b270cc1afe5ca3ed7a46beec284858788689d575
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-07-15 22:43:04 -0400

    test

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require("path");
-app.use(express.static(path.join(__dirname, "./client/dist")));
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
@@ -9,6 +8,8 @@ const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "./client/dist")));
 
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
