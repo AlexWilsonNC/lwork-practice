87c36be2f648d9338777658a487a9e712761540e
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-07-29 23:40:55 -0400

    curl test

--- server.js
+++ server.js
@@ -196,28 +196,23 @@ app.get('/api/cards', async (req, res) => {
 });
 
 app.get('/api/cards/searchbyname/:name', async (req, res) => {
-  const cardName = req.params.name.toLowerCase();
+  const cardName = req.params.name.trim().toLowerCase(); // Normalize card name
   try {
     const collections = await cardConnection.db.listCollections().toArray();
-    console.log(`Found collections: ${collections.map(col => col.name)}`);
     let foundCards = [];
 
     for (const collectionInfo of collections) {
-      console.log(`Searching in collection: ${collectionInfo.name}`);
       const collection = cardConnection.collection(collectionInfo.name);
-      const cards = await collection.find({ name: new RegExp(`^${cardName}$`, 'i') }).toArray();
+      const cards = await collection.find({ name: new RegExp(`^${cardName}$`, 'i') }).toArray(); // Case-insensitive search
       foundCards = foundCards.concat(cards);
     }
 
     if (foundCards.length > 0) {
-      console.log(`Total found cards: ${foundCards.length}`);
       res.status(200).json(foundCards);
     } else {
-      console.log(`Card not found with name: ${cardName}`);
-      res.status(404).json({ message: `Card not found with name: ${cardName}` });
+      res.status(404).json({ message: `Card not found with name: ${req.params.name}` });
     }
   } catch (error) {
-    console.error('Error fetching card by name:', error);
     res.status(500).json({ message: 'Error fetching card by name' });
   }
 });
