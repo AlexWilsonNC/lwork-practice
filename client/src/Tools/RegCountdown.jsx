c5e2f3dea3f217ae4cc4664ecbf6b388e7eb0aec
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-09-24 21:38:36 -0400

    gh

--- client/src/Tools/RegCountdown.jsx
+++ client/src/Tools/RegCountdown.jsx
@@ -6,7 +6,7 @@ const TooltipContainer = styled.div`
   color: #fff;
   text-align: center;
   border-radius: 4px;
-  padding: 5px;
+  padding: 2px 3px;
   position: absolute;
   z-index: 2;
   bottom: 105%;
@@ -15,30 +15,55 @@ const TooltipContainer = styled.div`
   white-space: nowrap;
   font-size: 12px;
   @media screen and (max-width: 530px) {
-    font-size: 8px;  
+    font-size: 8px;
     bottom: 95%;
   }
 `;
 
+const RegistrationTime = styled.div`
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
+
 const CountdownTooltip = ({ registrationTime }) => {
   const [timeRemaining, setTimeRemaining] = useState('');
+  const [formattedRegTime, setFormattedRegTime] = useState('');
+  const [isFutureEvent, setIsFutureEvent] = useState(false);
 
   useEffect(() => {
+    const formatRegistrationTime = () => {
+      const registrationDate = new Date(registrationTime);
+      const options = { 
+        weekday: 'short', 
+        year: 'numeric', 
+        month: 'short', 
+        day: 'numeric', 
+        hour: 'numeric', 
+        minute: 'numeric',
+        timeZoneName: 'short' 
+      };
+      const formattedTime = registrationDate.toLocaleDateString(undefined, options);
+      setFormattedRegTime(formattedTime);
+    };
+
     const calculateTimeRemaining = () => {
       const now = new Date();
       const registrationDate = new Date(registrationTime);
       const difference = registrationDate - now;
 
       if (difference > 0) {
+        setIsFutureEvent(true);
         const days = Math.floor(difference / (1000 * 60 * 60 * 24));
         const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
         setTimeRemaining(`${days}d ${hours}h ${minutes}m`);
       } else {
-        setTimeRemaining(''); // Clear timeRemaining if registration time is in the past
+        setIsFutureEvent(false); // Clear if registration time is in the past
+        setTimeRemaining('');
       }
     };
 
+    formatRegistrationTime();
     calculateTimeRemaining();
     const intervalId = setInterval(calculateTimeRemaining, 60000); // Update every minute
 
@@ -50,8 +75,12 @@ const CountdownTooltip = ({ registrationTime }) => {
       <span className="material-symbols-outlined reg-icon">
         {new Date(registrationTime) > new Date() ? 'schedule' : 'edit_note'}
       </span>
-      {timeRemaining && <TooltipContainer>{timeRemaining}</TooltipContainer>}
-      {/* {timeRemaining && <TooltipContainer>Delayed</TooltipContainer>} */}
+      {isFutureEvent && (
+        <TooltipContainer>
+          {formattedRegTime && <RegistrationTime>{formattedRegTime}</RegistrationTime>}
+          {timeRemaining}
+        </TooltipContainer>
+      )}
     </div>
   );
 };
