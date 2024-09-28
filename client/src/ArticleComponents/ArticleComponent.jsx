7d05cf96da35fb2c357c1e2281378c20c5f77cb4
AlexWilsonNC <alexwilson0017@yahoo.com>
2024-09-27 23:02:36 -0400

    article

--- client/src/ArticleComponents/ArticleComponent.jsx
+++ client/src/ArticleComponents/ArticleComponent.jsx
@@ -5,6 +5,15 @@ import SidebarComponent from '../SideContent/SideContent'
 import article1 from '../Articles/whenModernBecomesRetro';
 import article2 from '../Articles/forgottenFormat';
 import '../css/articles.css';
+import fluffychompImg from '../assets/article-images/fluffychomp.png';
+import tinaimg from '../assets/article-images/13498118540092.jpg';
+import sixprizesscreensho from '../assets/article-images/sixprizes-2.0-2012-04-19-crop.webp';
+import pookahoohimg from '../assets/article-images/pumpupsmash.png';
+import pokecatcher from '../assets/article-images/en_US-BW2-095-pokemon_catcher.jpg';
+import darkraiHydreigon from '../assets/article-images/hydreigondarkrai.png';
+import terrakionmewtwodar from '../assets/article-images/terrakionmewtwodar.png';
+import rayeels from '../assets/article-images/rayeels.png';
+import empogor from '../assets/article-images/empogor.png';
 
 const ArticleContainer = styled.div`
     width: 100%;
@@ -33,9 +42,64 @@ const ArticleComponent = () => {
     };
 
     const selectedArticle = articles[articleId];
-    console.log('Selected article:', selectedArticle);
+    if (selectedArticle) {
+      // Replace multiple placeholders with corresponding image tags
+      let updatedContent = selectedArticle.content
+        .replace(
+          '{{fluffychompImage}}',
+          `<a href='https://alexwilsonnc.github.io/tes/decks-by-era/2012/bw-drx' target='_blank'>
+            <img class='decklist-image' src='${fluffychompImg}' alt='FluffyChomp Deck Image' />
+          </a><br>`
+        )
+        .replace(
+          '{{tinasetImage}}',
+          `<a href='https://alexwilsonnc.github.io/tes/decks-by-era/2012/bw-drx' target='_blank'>
+          <img class='decklist-image' src='${tinaimg}' alt='article Image' /></a>
+          <br>`
+        )
+        .replace(
+          '{{pookahooh}}',
+          `<a href='https://alexwilsonnc.github.io/tes/decks-by-era/2012/bw-drx' target='_blank'>
+          <img class='decklist-image' src='${pookahoohimg}' alt='article Image' /></a>
+          <br>`
+        )
+        .replace(
+          '{{pokemoncatcher}}',
+          `<img class='article-pokemon-card-med' src='${pokecatcher}' alt='article Image' />`
+        )
+        .replace(
+          '{{darkraiHydreigonImg}}',
+          `<a href='https://alexwilsonnc.github.io/tes/decks-by-era/2012/bw-drx' target='_blank'>
+          <img class='decklist-image' src='${darkraiHydreigon}' alt='article Image' /></a>
+          <br>`
+        )
+        .replace(
+          '{{empogor}}',
+          `<a href='https://alexwilsonnc.github.io/tes/decks-by-era/2012/bw-drx' target='_blank'>
+          <img class='decklist-image' src='${empogor}' alt='article Image' /></a>
+          <br>`
+        )
+        .replace(
+          '{{terrakionmewtwodar}}',
+          `<a href='https://alexwilsonnc.github.io/tes/decks-by-era/2012/bw-drx' target='_blank'>
+          <img class='decklist-image' src='${terrakionmewtwodar}' alt='article Image' /></a>
+          <br>`
+        )
+        .replace(
+          '{{rayeels}}',
+          `<a href='https://alexwilsonnc.github.io/tes/decks-by-era/2012/bw-drx' target='_blank'>
+          <img class='decklist-image' src='${rayeels}' alt='article Image' /></a>
+          <br>`
+        )
+        .replace(
+          '{{sixprizesscreenshot}}',
+          `<a href='https://alexwilsonnc.github.io/tes/decks-by-era/2012/bw-drx' target='_blank'>
+          <img class='decklist-image' src='${sixprizesscreensho}' alt='article Image' /></a>
+          <br>`
+        );
 
-    setArticle(selectedArticle);
+      setArticle({ ...selectedArticle, content: updatedContent });
+    }
   }, [articleId]);
 
   if (!article) {
