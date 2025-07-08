2ce7c492e895654bbdcb7b7530123c560608266f
Alex <alexwilson0017@yahoo.com>
2025-07-08 19:13:02 -0400

    twitter test

--- client/src/SideContent/TweetFeed.jsx
+++ client/src/SideContent/TweetFeed.jsx
@@ -1,47 +1,31 @@
-import React, { useEffect, useRef } from 'react';
-import styled from 'styled-components';
-import { useTheme } from '../contexts/ThemeContext';
+import React, { useEffect } from 'react';
 
-const TwitterTimelineWrapper = styled.div`
-  .twitter-timeline {
-    ${({ theme }) => theme.twitterTimeline};
-  }
-`;
+const TweetFeed = () => {
+  useEffect(() => {
+    // 1) Inject widgets.js once
+    if (!window.twttr) {
+      const script = document.createElement('script');
+      script.src = 'https://platform.twitter.com/widgets.js';
+      script.async = true;
+      document.body.appendChild(script);
+    } else {
+      // 2) On subsequent loads just re-render the embed
+      window.twttr.widgets.load();
+    }
+  }, []);
 
-const TwitterWidget = () => {
-    const { theme } = useTheme();
-    const containerRef = useRef(null);
-
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = "https://platform.twitter.com/widgets.js";
-        script.async = true;
-        script.charset = 'utf-8';
-
-        // Append script only once
-        if (!window.twttr) {
-            document.body.appendChild(script);
-        } else {
-            // If already loaded, manually load the widget in this container
-            window.twttr.widgets.load(containerRef.current);
-        }
-    }, []);
-
-    return (
-        <TwitterTimelineWrapper theme={theme} className='homepage-hide-large'>
-            <div ref={containerRef}>
-                <a
-                    className="twitter-timeline"
-                    data-width="450"
-                    data-height="500"
-                    data-theme={theme.themeName}
-                    href="https://twitter.com/PTCG_Legends?ref_src=twsrc%5Etfw"
-                >
-                    Tweets by PTCG_Legends
-                </a>
-            </div>
-        </TwitterTimelineWrapper>
-    );
+  // 3) This blockquote is what Twitter will turn into your timeline
+  return (
+    <blockquote
+      className="twitter-timeline"
+      data-theme="light"      // or "dark", if you prefer
+      data-height="500"
+    >
+      <a href="https://twitter.com/PTCG_Legends?ref_src=twsrc%5Etfw">
+        Tweets by @PTCG_Legends
+      </a>
+    </blockquote>
+  );
 };
 
-export default TwitterWidget;
+export default TweetFeed;
