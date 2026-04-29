d2ba12317c9f215bbe21f16edc51546264f5f939
Alex <alexwilson0017@yahoo.com>
2026-04-28 20:43:26 -0400

    engy fix

--- client/src/Tournaments/PlayerDeck.jsx
+++ client/src/Tournaments/PlayerDeck.jsx
@@ -51,7 +51,7 @@ const PlayerDeckCenter = styled.div`
 `;
 
 const orderedSets = [
-    "POR", "ASC", "PFL", "MEG", "MEE", "MEP", "BLK", "WHT", "DRI", "JTG", "PRE", "SSP", "SCR", "SFA", "TWM", "TEF", "PAF", "PAR", "MEW", "OBF", "PAL", "SVI", "SVE", "PR-SV",
+    "POR", "ASC", "PFL", "MEG", "MEE", "MEP", "BLK", "WHT", "DRI", "JTG", "PRE", "SSP", "SCR", "SFA", "TWM", "TEF", "PAF", "PAR", "MEW", "OBF", "PAL", "SVE", "SVI", "PR-SV",
     "CRZ", "SIT", "LOR", "PGO", "ASR", "BRS", "FST", "CEL", "EVS", "CRE", "BST",
     "SHF", "VIV", "CPA", "DAA", "RCL", "SSH", "PR-SW",
     "CEC", "HIF", "UNM", "UNB", "DET", "TEU", "LOT", "DRM", "CES", "FLI", "UPR",
