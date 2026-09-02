45024ae2258e840000bc8f9971a51871406348ac
Alex <alexwilson0017@yahoo.com>
2026-09-02 10:21:08 -0400

    side

--- client/src/SideContent/SideContent.jsx
+++ client/src/SideContent/SideContent.jsx
@@ -78,11 +78,11 @@ const SidebarComponent = () => {
       <div className='side-section'>
         <h4>Popular Pages:</h4>
         <ol>
-          <li><Link to='/deckbuilder'>Deck Builder</Link></li>
-          <li><Link to='/player/tord-reklev-NO'>Tord Reklev's Player Page</Link></li>
-          <li><Link to='/tournaments/2026_NAIC'>2026 NAIC Results</Link></li>
-          <li><Link to='/player/ross-cawthon-US'>Ross Cawthon's Player Page</Link></li>
+          <li><Link to='/tournaments/2026_Worlds'>2026 Worlds Results</Link></li>
+          <li><Link to='/player/andrew-hedrick-US'>Andrew Hedrick's Player Page</Link></li>
+          <li><Link to='/player/diego-cassiraga-AR'>Diego Cassiraga's Player Page</Link></li>
           <li><Link to='/player/jason-klaczynski-US'>Jason Klaczynski's Player Page</Link></li>
+          <li><Link to='/tournaments/2026_LOS_ANGELES/masters'>2026 Los Angeles Results</Link></li>
         </ol>
       </div>
 
